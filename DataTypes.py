ceaf77401afdce4ce2edea1062cc29c7ad71db4a
vijaygovindu <govinduvijaykumar123@gmail.com>
2024-04-10 17:16:17 +0530

    DataTypes

--- DataTypes.py
+++ DataTypes.py
@@ -35,8 +35,72 @@ print("After Conversion")
 for i in range(len(f)):
     print(f[i])
 print(type(f))
-f[0]=100
+#f[0]=100 Since Bytes are immutable does not support item assignment
+
 #Bytearray is mutable we can change values
 g=[40,30,20,10]
 g=bytearray(g)
-print(g)
+for i in range(0,4):
+    print(g[i],end=",")
+print()
+g[0]=35
+for i in range(0,4):
+    print(g[i],end=",")
+print()
+#Range :Three ways to write the range function
+#Syntax:1
+for i in range(10):#Prints the indexes from 0-9
+    print(i,end=",") #Prints indexes from 0-9
+print()
+for i in range(0,10):
+    print(i,end=",")
+print()
+for i in range(0,10,2):
+    print(i,end=",")
+print()
+
+#9.List:List is a homogenious data structure which stores any one particular data type
+h=[10,"True",12.5]#Both Homogenious and Heterogenious data types are speciallly allowed in python.
+print(type(h))#returns list
+print(h)
+h[2]="False"
+print(h)
+
+#10.Tuple :Tuple is a heterogenious data structure and immutable in nature"
+#Ways to Declare Tuple using () and without using {()}
+#Syntax-1
+i=(20,"Tuple",2.5,"False")
+print(i)
+print(type(i))
+#i[1]="Tuple data Type"
+#print("The error is ",i)
+#Syntax -2
+i1=(3.3,"False","String")
+print(type(i1))
+
+# #11:Set is a non-homogenious data structures and mutable in nature and does not
+# allow duplicates to store.
+j={"Vijay","Spoo","Sai Chamakuri",10,3.2,"False"}
+print(j,type(j))
+
+
+#12:FrozenSet:Frozenset is immutable and remainig all other rules are same as set
+k=frozenset(j)
+print(j,type(k))
+
+#13;Dictionary:Dict is used to store key value pairs
+#Dictionary uses Key-Value Pairs to store values and keys are unique
+#Insertion order is not preserved
+#It is immutables
+l={1:"Vijay",2.0:"SAi",3.9:"Spoorthi"}
+print(l)
+l[2]="Chamakuri"
+print(l)
+
+#14:None
+m=None
+print(m,type(m))
+
+
+
+
