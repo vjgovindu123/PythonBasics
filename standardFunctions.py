2a17f269ae3b6d9c167d38e1dad7c3fd13d7239c
vijaygovindu <govinduvijaykumar123@gmail.com>
2024-04-13 10:20:17 +0530

    standard Functions in Python

---
+++ standardFunctions.py
@@ -0,0 +1,23 @@
+#Standard Functions
+"""
+1.all():if all the elements are true then it is true
+2.any():if any one element is true then entire array is true
+3.enumerate():it is another way of iterating elements or objects
+4.list():list is used to accept any time of data
+5.min():min is used to retrieve the least element in array
+6.max():max is used to retrieve the max element in an array
+7.sum():sum is used to summate the entire array elements
+8.sort():sort is used to arrange the elements in either in ascending or descending order
+9.len():len is used to print the lenght of array or size of array
+"""
+a=[10,20,30,0]
+print("The all() function is :",all(a))
+print("The any() function is:",any(a))
+print("The enumerate() function is:",enumerate(a),list(a))
+print("The list() is:",list(a))
+print("The min() is:",min(a))
+print("The max() is:",max(a))
+print("The sum() is:",sum(a))
+print("The sort() is:",sorted(a))
+print("The len() is:",len(a))
+
