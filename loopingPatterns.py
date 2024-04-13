f5b770b320d191e4afd5a305091a8989a80cbae6
vijaygovindu <govinduvijaykumar123@gmail.com>
2024-04-13 10:18:06 +0530

    loopingupdated1

--- loopingPatterns.py
+++ loopingPatterns.py
@@ -1,17 +1,20 @@
-#Pattern-1 :Stars Printing
+#Pattern-1 :Stars Printing in Square Format
+print("Pattern-1")
 n=int(input("Enter n Value"))
 while(n!=0):
-    print("*"*5)
+    print("*"*n)
     n=n-1
-print("Outside While")
-#Pattern-2:Numbers Printing RowWise
-print("pattern2")
+
+#Pattern-2:Numbers Printing RowWise in Square Format
+print("pattern-2")
 n=int(input("Enter N value"))
 for i in range(1,n+1):
     for j in range(1,n+1):
         print(i,end="")
     print()
+
 #pattern-3:Numbers Printing RowWise with indexing
+print("Pattern-3")
 for i in range(1,n+1):
     for j in range(0,i):
         print(i,end="")
@@ -39,9 +42,25 @@ for i in range(1,n+1):
     # print()
     """
 
-#pattern-5:
+#pattern-4:Numbers Printing in Column Wise
+print("Pattern-4")
 n=int(input(" enter n"))
 for i in range(1,n):
     for j in range(1,i+1):
         print(j,end=" ")
     print()
+
+#Pattern-5: Stars Printing in increasing order in right angled triangle.
+
+n=int(input("Enter n"))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(i,end="")
+    for j in range(1,n-i+1):
+        print("_",end="")
+    for j in range(1,n-i+1):
+       print("_",end="")
+    for j in range(1,i+1):
+       print(i,end="")
+    print()
+
